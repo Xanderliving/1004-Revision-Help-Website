726b22d3c10aa0b547cedabda06b4ab72dcf9f82
Xanderliving <91468594+Xanderliving@users.noreply.github.com>
2023-02-17 01:05:11 +0000

    Webpage

---
+++ js/json.js
@@ -0,0 +1,65 @@
+﻿let Work = [];
+function onclick_submit() {
+        //Puts info into array
+        let Subject = {
+            Subject: document.getElementById('Subject').value,
+            Importance: document.getElementById('Importance').value,
+            Due: document.getElementById('Due').value
+        }
+    
+    //Clears previouse info
+    document.forms[0].reset();
+
+    //Pushes Info
+    Work.push(Subject);
+
+    //Tells console info is added
+    console.warn('added', { Work });
+
+    //Saves to localStorage
+        localStorage.setItem('MyWorkList', JSON.stringify(Work));
+    
+    //Finds table
+    var table = document.getElementById("myTable");
+
+    //Creates empty <tr>
+    var row = table.insertRow(1);
+
+    //Inserts new cells 
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+
+    //Adds info to the cells
+    for (let i = 0; i <= Work.length - 1; i++) {
+
+        cell1.innerHTML = Work[i].Subject;
+        cell2.innerHTML = Work[i].Importance;
+        cell3.innerHTML = Work[i].Due;
+
+    }
+}
+let Game = [];
+function onclick_submit2() {
+
+    //Puts info into array
+    let Data = {
+        Username: document.getElementById('Username').value,
+        Application: document.getElementById('Application').value,
+        PlayMins: document.getElementById('PlayMins').value,
+        Score: document.getElementById('Score').value,
+        Level: document.getElementById('Level').value
+    }
+
+    //Clears previouse info
+    document.forms[0].reset();
+
+    //Pushes Info
+    Game.push(Data);
+
+    //Tells console info is added
+    console.warn('added', { Game });
+
+    //Saves to localStorage
+    localStorage.setItem('Default', JSON.stringify(Game));
+}
---
+++ js/randomiser.js
@@ -0,0 +1,28 @@
+﻿
+function onclick_Randomiser() {
+    //Picks time
+    var number = parseInt(Math.floor(Math.random() * 180) + 30);
+    //Picks random Subject
+    Array = Math.floor(Math.random() * Work.length);
+    //Displays random time & subject
+    document.getElementById("RSubject").innerHTML = "Random Subject: " + Work[Array].Subject;
+    document.getElementById("RTime").innerHTML = number;
+}
+function onclick_Countdown() {
+    //Gets random time
+    Countdown = document.getElementById("RTime").innerHTML;
+    //Times it by 60 to get seconds
+    var Timeleft = Countdown * 60;
+    //Displays finnished or counts down more
+    var Timer = setInterval(
+        function () {
+        if (Timeleft <= 0) {
+            clearInterval(Timer);
+            alert("Finished");
+            document.getElementById("countdown").innerHTML = "Finished";
+        } else {
+            document.getElementById("countdown").innerHTML = Timeleft + " seconds remaining";
+        }
+        Timeleft -= 1;
+    }, 1000);
+}
